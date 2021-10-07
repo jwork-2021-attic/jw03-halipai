a7ffe10d3cc4362065ea2a3e1398aa5ab8b03c18
halipai <81256030+halipai@users.noreply.github.com>
2021-10-07 13:31:22 +0800

    Delete jw03-halipai directory

--- jw03-halipai/example/encoder/DecodingException.java
+++
@@ -1,8 +0,0 @@
-package example.encoder;
-
-public class DecodingException extends Exception {
-
-    public DecodingException(String param, int value) {
-        super("Error while decoding! " + param + " has invalid value of: " + value);
-    }
-}
